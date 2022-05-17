7dca33dacbc68e8c2076edad3cdcd8151c5f69ba
Gabriel Linhares <glinharesb@gmail.com>
2022-05-17 17:53:52 -0300

    fix(products): adjust in api

--- src/helpers/products.ts
+++ src/helpers/products.ts
@@ -2,24 +2,22 @@ import { IProducts } from '../components/Context'
 
 export async function getProducts() {
   try {
-    const response = await fetch('/api/products')
-    const data = response.json()
+    const data = localStorage.getItem('products')
+    const parsedData = !data ? [] : JSON.parse(data)
 
-    return data
+    return parsedData
   } catch (error) {
     console.error(error)
   }
+
+  return []
 }
 
 export async function addProduct(product: IProducts) {
   try {
-    await fetch('/api/products', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(product)
-    })
+    const products = await getProducts()
+
+    localStorage.setItem('products', JSON.stringify([...products, product]))
   } catch (error) {
     console.error(error)
   }
@@ -27,13 +25,14 @@ export async function addProduct(product: IProducts) {
 
 export async function removeProduct(uuid: number) {
   try {
-    await fetch('/api/products', {
-      method: 'DELETE',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({ uuid })
-    })
+    const products = await getProducts()
+    if (!products.length) return
+
+    const filteredProducts = products.filter(
+      (product: IProducts) => product.uuid !== uuid
+    )
+
+    localStorage.setItem('products', JSON.stringify(filteredProducts))
   } catch (error) {
     console.error(error)
   }
